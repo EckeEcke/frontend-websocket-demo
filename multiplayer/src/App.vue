754c30283a3cbb2b552029c7bd6b084395e8a8b6
Christian Eckardt <63042360+EckeEcke@users.noreply.github.com>
2024-01-19 13:27:44 +0100

    Update App.vue

--- multiplayer/src/App.vue
+++ multiplayer/src/App.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
-import {ref} from "vue"
+import {computed, ref} from "vue"
 import {type Ref} from "vue"
 import MarioAnimation from './components/MarioAnimation.vue'
 
 const serverUrl = 'wss://websocket-multiplayer-demo.onrender.com'
 const socket = new WebSocket(serverUrl)
+const isPlayer1 = computed(() => player.value === 1)
+const isPlayer2 = computed(() => player.value === 2)
 const clicks2 = ref(0)
 const clicks1 = ref(0)
-const player = ref(undefined)
+const player: Ref<number | undefined> = ref(undefined)
 const state = ref('WAITING')
 const winner: Ref<boolean | undefined> = ref(undefined)
 
 const musicPlaying = ref(false)
 const music = new Audio('/music.mp3')
+const countDown = ref(3)
+let countDownInterval = ref(false)
+const runCountDown = () => {
+  if(countDown.value > 0) {
+    countDown.value--
+  } else {
+    clearInterval(countDownInterval.value)
+    countDownInterval.value = false
+  }
+}
 
 socket.addEventListener('message', function (event) {
   const data = JSON.parse(event.data)
@@ -22,15 +34,17 @@ socket.addEventListener('message', function (event) {
   if(data.state) state.value = data.state
   if(state.value === 'GAME_READY' || state.value === 'GAME_RUNNING') winner.value = undefined
   if(state.value === 'GAME_READY') {
+    countDown.value = 3
     clicks1.value = 0
     clicks2.value = 0
+    if(!countDownInterval.value) countDownInterval.value = setInterval(() => runCountDown(), 1000)
   }
   if(state.value === 'GAME_RUNNING' && !musicPlaying.value) {
     musicPlaying.value = true
     music.currentTime = 0
     music.play()
-
   }
+  if(data.message === 'USER LEFT') location.reload()
   getWinner()
 })
 
@@ -39,7 +53,12 @@ socket.addEventListener('open', function(){
 })
 
 let clicks = ref(0)
-const addClick = (id: string) => { clicks.value++; socket.send(id) }
+const addClick = (id: string) => {
+  if (countDown.value === 0) {
+    clicks.value++;
+    socket.send(id)
+  }
+}
 const retryGame = () => {
   socket.send('RETRY')
 }
@@ -55,20 +74,23 @@ function getWinner() {
   <h1>Super Mario<br>Clicky Clicky</h1>
   <div class="scoreboard">
     <div>
+      <div class="you" :class="{'hidden': isPlayer2}">&#128315;</div>
       <h2>P1</h2>
       <div class="scoredisplay">{{ clicks1 }}</div>
     </div>
     <div>
+      <div class="you" :class="{'hidden': isPlayer1}">&#128315;</div>
       <h2>P2</h2>
       <div class="scoredisplay">{{ clicks2 }}</div>
     </div>
   </div>
   <div class="race-wrapper">
+    <div class="countdown" :class="{'fade-out': countDown === 0}">{{ countDown }}</div>
     <div class="mario-wrapper">
       <MarioAnimation :is-player-2="false" :clicks="clicks1" />
       <MarioAnimation :is-player-2="true" :clicks="clicks2" />
     </div>
-    <img class="finish-line" src="/finishline.png#">
+    <img class="finish-line" src="/finishline.png">
     <div class="finish"></div>
     <div class="message">
       <h2 v-if="winner === true">YOU WON!</h2>
@@ -108,9 +130,18 @@ h2 {
   color: white;
   font-weight: bolder;
 }
-button, .scoredisplay {
+button, .scoredisplay, .you {
   font-weight: bolder;
   text-align: center;
+}
+
+.you {
+  font-size: 16px;
+  color: red;
+}
+
+.scoredisplay {
+  font-size: 24px;
 }
 
 img {
@@ -210,6 +241,27 @@ button {
 
 .player {
   margin-top: 16px;
+}
+
+.countdown {
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1;
+  font-size: 64px;
+  color: red;
+  text-shadow: -2px -2px 0 yellow, 2px -2px 0 yellow, -2px 2px 0 yellow, 2px 2px 0 yellow;
+}
+
+.hidden {
+  opacity: 0;
+}
+
+.fade-out {
+  visibility: hidden;
+  opacity: 0;
+  transition: visibility 0s 2s, opacity 2s linear;
 }
 
 @keyframes blink {
